0aaf15dbdd60d69660f95012c780c3bfa96415df
RahulBayad <rahulbayad05@gmail.com>
2024-05-30 12:43:42 +0530

    db error solving

--- src/db/index.js
+++ src/db/index.js
@@ -3,8 +3,8 @@ import { DB_NAME } from "./../constants.js";
 
 const dbConnect = async() =>{
     try {
-        let connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`/${DB_NAME})
-        console.log(`\n MongoDB connected !! DB HOST ${connectionInstance.connection.host}`)
+        let connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\nMongoDB connected !! DB HOST ${connectionInstance.connection.host}`)
     } catch (error) {
         console.log("MongoDB connection failed", error)
         process.exit(1)
