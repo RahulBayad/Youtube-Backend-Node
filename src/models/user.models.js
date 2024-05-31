518842008b102879551e3acaf9f2f19b3122fac1
RahulBayad <rahulbayad05@gmail.com>
2024-05-31 13:42:06 +0530

    Bcrypt updated in pre-save method

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -54,13 +54,13 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next();
-
-    this.password = await bcrypt.hash(this.password , 10)
+    const salt = bcrypt.genSaltSync(10)
+    this.password = await bcrypt.hash(this.password , salt)
     next();
 })
 
 userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare( password , this.password)
+    return await bcrypt.compare( password , this.password)  
 }
 
 userSchema.methods.generateRefreshToken = async function(){
