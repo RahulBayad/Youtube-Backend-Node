e05d78b56f3196fb643715c702422306f088e7ea
RahulBayad <rahulbayad05@gmail.com>
2024-06-01 17:16:21 +0530

    Created video schema

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,7 +1,42 @@
 import mongoose, {Schema} from "mongoose";
 
-const videoSchema = new Schema({
-    
-})
+const videoSchema = new Schema(
+    {
+        videoFile : {
+            type : String,
+            required : true
+        },
+        title : {
+            type : String,
+            required : true
+        },
+        thumbnail : {
+            type : String,
+        },
+        description : {
+            type : String,
+            required : true
+        },
+        duration : {
+            type : Number,
+            required : true
+        },
+        views : {
+            type : Number,
+            required : true
+        },
+        isPublished : {
+            type : Boolean,
+            default : true
+        },
+        owner : {
+            type : Schema.Types.ObjectId,
+            ref : "User"
+        }   
+    },
+    {
+        timestamps : true
+    }
+)
 
 export const Video = mongoose.model("Video",videoSchema)
