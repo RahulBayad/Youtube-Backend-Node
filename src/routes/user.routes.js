24c7289f951fbb469aab0a2a0fa469bf5763ad05
RahulBayad <rahulbayad05@gmail.com>
2024-06-01 17:09:04 +0530

    routes updated

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -13,6 +13,7 @@ import {
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { jwtVerify } from "../middlewares/auth.middleware.js";
+import multer from "multer";
 
 const router = Router()
 
@@ -22,9 +23,9 @@ router.route("/logout").post(jwtVerify, logoutUser)
 router.route("/refresh-token").post(refreshTheAccessToken)
 router.route("/change-password").post(jwtVerify, changePassword)
 router.route("/current-user").get(jwtVerify, getCurrentUser)
-router.route("/update-account").post(jwtVerify, updateAccountDetails)
-router.route("/avatar").post(jwtVerify, updateUserAvatar)
-router.route("/coverimage").post(jwtVerify, updateUserCoverImage)
-router.route("/channel/:username").get(getUserChannelProfile)
+router.route("/update-account").patch(jwtVerify, updateAccountDetails)
+router.route("/avatar").patch(jwtVerify, upload.single("avatar"), updateUserAvatar)
+router.route("/coverimage").patch(jwtVerify, upload.single("coverImage"), updateUserCoverImage)
+router.route("/channel/:username").get( jwtVerify, getUserChannelProfile)
 
 export default router
